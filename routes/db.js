1a7fa3b9e86b2f1e09cadac5e7192558e6c87a4a
e-dot <e@kartmann.org>
2016-03-31 20:21:45 +0200

    factorisation of db code for reuse in other forms

--- routes/db.js
+++ routes/db.js
@@ -53,13 +53,76 @@ var check_field_value = function(strValue, arrFieldDescription, objSQLConnection
   return(strSQLValue);
 }
 
-var arrExceptFields = [
+var form_ignore_fields = [
   "OK",
   "CANCEL"
 ];
 
+var new_record = function(req, res, next, objFormParameters)
+{
+  // Must be something in the body
+  if (!req.body) return res.sendStatus(400);
+
+  var objSQLConnection = new_connection();
+/* DEBUG
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('you posted:\n');
+  res.end(JSON.stringify(req.body, null, 2));
+*/
+/* DEBUG */
+
+  var arrSQLNames = new Array();
+  var arrSQLValues = new Array();
+  for (var strSentName in req.body)
+  {
+    // DEBUG console.log("strSentName=\""+strSentName+"\"\n");
+    // Dont look for excluded fields, like "OK" or "CANCEL"
+    if (form_ignore_fields.indexOf(strSentName) == -1)
+    {
+      var objField = null;
+      for (var intField = 0; intField < objFormParameters.fields.length; intField++)
+      {
+        var strFieldName = objFormParameters.fields[intField].name;
+        if (strFieldName)
+        {
+          // DEBUG console.log("strFieldName=\""+strFieldName+"\"\n");
+          if (strSentName == strFieldName.valueOf())
+          {
+            // Known field : store attributes to check value and prepare storage
+            objField = objFormParameters.fields[intField];
+          } // if (strSentName == strFieldName.valueOf())
+        } // if (strFieldName)
+      } // for (var intField = 0; intField < objFormParameters.fields; intField++)
+      if (objField == null)
+      {
+        // Unknown field: error!
+        throw new Error("ERROR: field \""+strSentName+"\" is unknown!");
+      } // if (objField == null)
+      else
+      {
+        var strSQLValue = check_field_value(req.body[strSentName],objField,objSQLConnection);
+        arrSQLNames.push(objSQLConnection.escapeId(objField.name)); // TODO handle name translation from HTML FORM to SQL TABLE COLUMN
+        arrSQLValues.push(strSQLValue);
+      } // else if (objField == null)
+    } // if (form_ignore_fields.indexOf(strSentName) == -1)
+  } // for (var objField in req.body)
+
+  var strSQL = "INSERT INTO "+objSQLConnection.escapeId(objFormParameters.table_name)+"("+arrSQLNames.join(",")+")\n"
+             + "VALUES("+arrSQLValues.join(",")+")\n"
+             + ";"
+             ;
+  // DEBUG
+  console.log(strSQL);
+
+  runsql(strSQL, function(err, rows, fields) {
+    // TODO handle duplicate key errors (login must be unique but it is provided by user)
+    if (err) throw err;
+    res.redirect('list'); // TODO res.redirect('record')
+  });
+}
 
 module.exports.new_connection = new_connection;
 module.exports.runsql = runsql;
 module.exports.check_field_value = check_field_value;
-module.exports.form_ignore_fields = arrExceptFields;
+module.exports.form_ignore_fields = form_ignore_fields;
+module.exports.new_record = new_record;
--- routes/user.js
+++ routes/user.js
@@ -39,65 +39,7 @@ router.get('/new', function(req, res, next) {
 });
 // POST new user (form validation)
 router.post('/new', function(req, res, next) {
-  var objSQLConnection = db.new_connection();
-/* DEBUG
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('you posted:\n');
-  res.end(JSON.stringify(req.body, null, 2));
-*/
-/* DEBUG */
-
-  // Must be something in the body
-  if (!req.body) return res.sendStatus(400);
-
-  var arrSQLNames = new Array();
-  var arrSQLValues = new Array();
-  for (var strSentName in req.body)
-  {
-    // DEBUG console.log("strSentName=\""+strSentName+"\"\n");
-    // Dont look for excluded fields, like "OK" or "CANCEL"
-    if (db.form_ignore_fields.indexOf(strSentName) == -1)
-    {
-      var objField = null;
-      for (var intField = 0; intField < objFormParameters.fields.length; intField++)
-      {
-        var strFieldName = objFormParameters.fields[intField].name;
-        if (strFieldName)
-        {
-          // DEBUG console.log("strFieldName=\""+strFieldName+"\"\n");
-          if (strSentName == strFieldName.valueOf())
-          {
-            // Known field : store attributes to check value and prepare storage
-            objField = objFormParameters.fields[intField];
-          } // if (strSentName == strFieldName.valueOf())
-        } // if (strFieldName)
-      } // for (var intField = 0; intField < objFormParameters.fields; intField++)
-      if (objField == null)
-      {
-        // Unknown field: error!
-        throw new Error("ERROR: field \""+strSentName+"\" is unknown!");
-      } // if (objField == null)
-      else
-      {
-        var strSQLValue = db.check_field_value(req.body[strSentName],objField,objSQLConnection);
-        arrSQLNames.push(objSQLConnection.escapeId(objField.name)); // TODO handle name translation from HTML FORM to SQL TABLE COLUMN
-        arrSQLValues.push(strSQLValue);
-      } // else if (objField == null)
-    } // if (db.form_ignore_fields.indexOf(strSentName) == -1)
-  } // for (var objField in req.body)
-
-  var strSQL = "INSERT INTO "+objSQLConnection.escapeId(objFormParameters.table_name)+"("+arrSQLNames.join(",")+")\n"
-             + "VALUES("+arrSQLValues.join(",")+")\n"
-             + ";"
-             ;
-  // DEBUG
-  console.log(strSQL);
-
-  db.runsql(strSQL, function(err, rows, fields) {
-    if (err) throw err;
-    res.redirect('list'); // TODO /record
-  });
-
+  db.new_record(req,res,next,objFormParameters);
 });
 
 // TODO search, delete
