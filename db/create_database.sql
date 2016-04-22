c080e4a80283fa0fc4c2bd5885a87f0c94c58621
e-dot <e@kartmann.org>
2016-04-22 16:05:00 +0200

    created triggers to update search engine tables on INSERT/UPDATE/DELETE

--- db/create_database.sql
+++ db/create_database.sql
@@ -54,24 +54,54 @@ CREATE TABLE item(
 ) ENGINE=INNODB COMMENT 'Items in the inventory (one row per instance)'
 ;
 
-DROP TABLE IF EXISTS item_search
+DROP TABLE IF EXISTS item_detail_search
 ;
-CREATE TABLE item_search(
+CREATE TABLE item_detail_search(
   id INTEGER NOT NULL AUTO_INCREMENT,
 
-  item_id INTEGER NOT NULL COMMENT 'Link to the item table',
+  item_detail_id INTEGER NOT NULL COMMENT 'Link to the item_detail table',
   title VARCHAR(255) NOT NULL COMMENT 'Item Title',
   author VARCHAR(255) NOT NULL COMMENT 'Item Author',
 
   PRIMARY KEY(id),
-  KEY item_search_title(title),
-  KEY item_author(author),
+  KEY item_detail_search_title(title),
+  KEY item_detail_search_author(author),
   FULLTEXT KEY item_ft_title(title),
   FULLTEXT KEY item_ft_author(author)
 
 )  ENGINE=MyISAM COMMENT 'Search engine for items (MyISAM Format for FULL TEXT SEARCHES)'
 ;
 
+/* Create trigger on item_detail to copy data into item_detail_search */
+SET @SAVE_SQL_MODE=@@SQL_MODE
+;
+DELIMITER ;;
+SET SESSION SQL_MODE="";;
+/* INSERT : After INSERT INTO table, INSERT INTO search table */
+DROP TRIGGER IF EXISTS `item_detail_after_insert`;;
+CREATE DEFINER=CURRENT_USER TRIGGER `item_detail_after_insert` AFTER INSERT ON `item_detail` 
+FOR EACH ROW BEGIN
+  INSERT INTO item_detail_search(item_detail_id, title, author) VALUES(NEW.id, NEW.title, NEW.author);
+END ;;
+/* UPDATE : After UPDATE table, UPDATE search table */
+DROP TRIGGER IF EXISTS `item_detail_after_update`;;
+CREATE DEFINER=CURRENT_USER TRIGGER `item_detail_after_update` AFTER UPDATE ON `item_detail` 
+FOR EACH ROW BEGIN
+  UPDATE item_detail_search SET 
+    item_detail_search.title = NEW.title, 
+    item_detail_search.author = NEW.author 
+  WHERE item_detail_search.item_detail_id = NEW.id;
+END ;;
+/* DELETE : After DELETE table, DELETE search table */
+DROP TRIGGER IF EXISTS `item_detail_after_delete`;;
+CREATE DEFINER=CURRENT_USER TRIGGER `item_detail_after_delete` AFTER DELETE ON `item_detail` 
+FOR EACH ROW BEGIN
+  DELETE FROM item_detail_search 
+  WHERE item_detail_search.item_detail_id = OLD.id;
+END ;;
+DELIMITER ;
+SET SESSION SQL_MODE=@SAVE_SQL_MODE
+;
 
 
 DROP TABLE IF EXISTS user
@@ -90,6 +120,56 @@ CREATE TABLE user(
   KEY user_phone(phone)
 
 ) ENGINE=INNODB COMMENT 'User description'
+;
+
+DROP TABLE IF EXISTS user_search
+;
+CREATE TABLE user_search(
+  id INTEGER NOT NULL AUTO_INCREMENT,
+
+  user_id INTEGER NOT NULL COMMENT 'Link to the user table',
+  name VARCHAR(255) NOT NULL COMMENT 'User name',
+  login VARCHAR(64) NOT NULL COMMENT 'User login',
+
+  PRIMARY KEY(id),
+  KEY user_search_name(name),
+  KEY user_search_login(login),
+  FULLTEXT KEY user_ft_name(name),
+  FULLTEXT KEY user_ft_login(login)
+
+)  ENGINE=MyISAM COMMENT 'Search engine for users (MyISAM Format for FULL TEXT SEARCHES)'
+;
+
+/* Create trigger on user to copy data into user_search */
+/* Create trigger on item_detail to copy data into item_detail_search */
+SET @SAVE_SQL_MODE=@@SQL_MODE
+;
+DELIMITER ;;
+SET SESSION SQL_MODE="";;
+/* INSERT : After INSERT INTO table, INSERT INTO search table */
+DROP TRIGGER IF EXISTS `user_after_insert`;;
+CREATE DEFINER=CURRENT_USER TRIGGER `user_after_insert` AFTER INSERT ON `user` 
+FOR EACH ROW BEGIN
+  INSERT INTO user_search(user_id, name, login) VALUES(NEW.id, NEW.name, NEW.login);
+END ;;
+/* UPDATE : After UPDATE table, UPDATE search table */
+DROP TRIGGER IF EXISTS `user_after_update`;;
+CREATE DEFINER=CURRENT_USER TRIGGER `user_after_update` AFTER UPDATE ON `user` 
+FOR EACH ROW BEGIN
+  UPDATE user_search SET 
+    user_search.name = NEW.name, 
+    user_search.login = NEW.login 
+  WHERE user_search.user_id = NEW.id;
+END ;;
+/* DELETE : After DELETE table, DELETE search table */
+DROP TRIGGER IF EXISTS `user_after_delete`;;
+CREATE DEFINER=CURRENT_USER TRIGGER `user_after_delete` AFTER DELETE ON `user` 
+FOR EACH ROW BEGIN
+  DELETE FROM user_search 
+  WHERE user_search.user_id = OLD.id;
+END ;;
+DELIMITER ;
+SET SESSION SQL_MODE=@SAVE_SQL_MODE
 ;
 
 
