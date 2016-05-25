da17a3442f2234bf3ce812b5e046fdf11b4afae1
e-dot <e@kartmann.org>
2016-05-25 14:08:11 +0200

    reverse order: stop, git pull, real upgrade, start

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -9,12 +9,12 @@ handle_error()
   exit 1
 }
 
+echo "[`date +'%Y-%m-%d %H:%M:%S'`] Stop service"
+bash bin/stop.sh
+
 echo "[`date +'%Y-%m-%d %H:%M:%S'`] Upgrade source code..."
 cd `dirname $0`/..
 git pull || handle_error "Can't run 'git pull'"
-
-echo "[`date +'%Y-%m-%d %H:%M:%S'`] Stop service"
-bash bin/stop.sh
 
 echo "[`date +'%Y-%m-%d %H:%M:%S'`] Upgrade software..."
 # TODO : upgrade database structure, if needed : execute db/alter/*.sql (only once - store alter exection in MySQL db)
