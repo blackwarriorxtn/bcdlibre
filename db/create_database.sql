f689add73cdd38d45bcd88958de29d1cb5d26801
e-dot <e@kartmann.org>
2016-05-25 19:23:11 +0200

    prepare autoupgrade/alter database

--- db/create_database.sql
+++ db/create_database.sql
@@ -275,3 +275,18 @@ CREATE TABLE log(
 
 ) ENGINE=MyISAM COMMENT 'Web Log'
 ;
+
+
+CREATE TABLE IF NOT EXISTS my_alter(
+  id INTEGER NOT NULL AUTO_INCREMENT,
+
+  commit_id VARCHAR(64) NOT NULL COMMENT 'Unique commit identifier for alter script',
+  alter_file VARCHAR(255) NOT NULL COMMENT 'Name of alter script file (.sql)',
+  commit_timestamp BIGINT NOT NULL COMMENT 'Timestamp of commit',
+  alter_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Date/Time of execution',
+
+  PRIMARY KEY(id),
+  UNIQUE KEY(commit_id,alter_file)
+
+) ENGINE=MyISAM COMMENT='Track database structure changes (alter)'
+;
