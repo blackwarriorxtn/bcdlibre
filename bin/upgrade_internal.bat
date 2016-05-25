f84bfb2a840a8d7c931befcec9417050ba8c4115
e-dot <e@kartmann.org>
2016-05-25 14:56:41 +0200

    upgrade node module on upgrade

--- bin/upgrade_internal.bat
+++ bin/upgrade_internal.bat
@@ -3,6 +3,8 @@ ECHO [%DATE% %TIME%] %~n0: Begin...
 
 cd /D %~dp0..\ || GOTO ERROR
 
+%COMSPEC% /C %~dp0..\setup\setup_node_modules.bat || GOTO ERROR
+
 REM TODO : upgrade database structure, if needed : execute db/alter/*.sql (only once - store alter exection in MySQL db)
 
 :END
--- bin/upgrade_internal.sh
+++ bin/upgrade_internal.sh
@@ -9,6 +9,8 @@ handle_error()
   exit 1
 }
 
+bash `dirname $0`/../setup/setup_node_modules.sh || handle_error "Can't install node modules!"
+
 cd `dirname $0`/..
 # TODO : upgrade database structure, if needed : execute db/alter/*.sql (only once - store alter exection in MySQL db)
 
