c85e2c4273fe252219d99056e4e4bc22ac56c21e
e-dot <e@kartmann.org>
2016-05-31 17:31:32 +0200

    added copyright licence everywhere

--- bin/backup.bat
+++ bin/backup.bat
@@ -1,6 +1,20 @@
 @ECHO OFF
-ECHO [%DATE% %TIME%] %~n0: Begin...
 
+REM       Copyright 2016 Replay SDK (http://www.replay-sdk.com)
+REM
+REM   Licensed under the Apache License, Version 2.0 (the "License");
+REM   you may not use this file except in compliance with the License.
+REM   You may obtain a copy of the License at
+REM
+REM      http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM   Unless required by applicable law or agreed to in writing, software
+REM   distributed under the License is distributed on an "AS IS" BASIS,
+REM   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM   See the License for the specific language governing permissions and
+REM   limitations under the License.
+
+ECHO [%DATE% %TIME%] %~n0: Begin...
 SET DB_NAME=bibliopuce
 CALL %~dp0date.bat
 REM Create directory tree %DB_NAME%-backup\%MY_YEAR%\%MY_MONTH%\%MY_DAY%
