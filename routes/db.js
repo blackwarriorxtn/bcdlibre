09cee6ec8dc6e1637d4aa0fbffb04420c364a31b
e-dot <e@kartmann.org>
2016-06-02 16:12:24 +0200

    fix isbn normalizer (way to aggresssive!)

--- routes/db.js
+++ routes/db.js
@@ -666,22 +666,33 @@ var format_isbn = function(strRawValue)
   {
     // Remove spaces at beginning/end
     strValue = strValue.replace(/^ +/g, "").replace(/ +$/g, "");
-    // Remove dashes and spaces - if string is longuer than 13
-    if (strValue.length > 13)
+
+    // Try to determine if string contains an ISBN or not
+    if (
+      /* Only numbers AND dashes */
+      strValue.match(/^[0-9-]+$/) ||
+      /* Only french keyboard letters for numbers (including dashes) */
+      strValue.match(/^[&é"'\(è_çà-]+$/)
+    )
     {
-      strValue = strValue.replace(/[ -]+/g, "");
+      // Remove dashes and spaces - if string is longuer than 13
+      if (strValue.length > 13)
+      {
+        strValue = strValue.replace(/[ -]+/g, "");
+      }
+      // Hack: some barcode readers mistype numbers/letters on (french) keyboard : fix it
+      strValue = strValue.replace(/&/g, "1")
+                         .replace(/é/g, "2")
+                         .replace(/"/g, "3")
+                         .replace(/'/g, "4")
+                         .replace(/\(/g, "5")
+                         /* TOO dangerous .replace(/-/g, "6") */
+                         .replace(/è/g, "7")
+                         .replace(/_/g, "8")
+                         .replace(/ç/g, "9")
+                         .replace(/à/g, "0");
     }
-    // Hack: some barcode readers mistype numbers/letters on (french) keyboard : fix it
-    strValue = strValue.replace(/&/g, "1")
-                       .replace(/é/g, "2")
-                       .replace(/"/g, "3")
-                       .replace(/'/g, "4")
-                       .replace(/\(/g, "5")
-                       .replace(/-/g, "6")
-                       .replace(/è/g, "7")
-                       .replace(/_/g, "8")
-                       .replace(/ç/g, "9")
-                       .replace(/à/g, "0");
+    // else: not an ISBN number, do nothing
   }
   return(strValue);
 }
