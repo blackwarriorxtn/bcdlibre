de1d6818b6c3039457e334c072771d22e00912ea
E-Dot <e@kartmann.org>
2016-09-14 17:43:24 +0200

    improved debug script (quiet no error message) fixed english l10n

--- bin/debug.sh
+++ bin/debug.sh
@@ -2,11 +2,11 @@
 
 cd `dirname $0`/..
 # Stop running process, if any
-bash bin/stop.sh
+bash bin/stop.sh 1>/dev/null 2>/dev/null
 # Enable debugging
 DEBUG=bibliopuce:*
 
 # Start application via node
+echo "Debugging application..."
 cd `dirname $0`/..
 node bin/www
-
