13cd11da91697c53bc56bf99da8513acfd993636
E-Dot <e@kartmann.org>
2016-09-16 19:16:51 +0200

    enabled full login

--- bin/debug.sh
+++ bin/debug.sh
@@ -4,7 +4,7 @@ cd `dirname $0`/..
 # Stop running process, if any
 bash bin/stop.sh 1>/dev/null 2>/dev/null
 # Enable debugging
-DEBUG=bibliopuce:*
+export DEBUG=bibliopuce:*
 
 # Start application via node
 echo "Debugging application..."
