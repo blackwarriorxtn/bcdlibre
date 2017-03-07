1107debef6ce122c0dbac31de0e556aad5ae2e64
e-dot <e@kartmann.org>
2017-03-07 09:53:00 +0100

    Fix full-text search of "stop words" that never find anything
    The default mysql full-text search "stop words list" contains words that
    MUST be indexed and searched in FRENCH language, like "ours" (bear) and
    "course" (race)
    => set an EMPTY full-text search by default for now

--- setup/setup.sh
+++ setup/setup.sh
@@ -43,6 +43,7 @@ bash `dirname $0`/setup_node_modules.sh || handle_error "Can't install node modu
 # Configure mysql
 if [ -f /etc/mysql/my.cnf ] ; then
   echo "[`date +'%Y-%m-%d %H:%M:%S'`] Configuring mysql..."
+  # ft_min_word_len : Set minimum word length for full-text search
   grep ft_min_word_len 1>/dev/null 2>/dev/null /etc/mysql/my.cnf
 
   if [ "$?" != "0" ] ; then
@@ -57,7 +58,24 @@ if [ -f /etc/mysql/my.cnf ] ; then
   else
     echo "[`date +'%Y-%m-%d %H:%M:%S'`] ft_min_word_len already configured in /etc/mysql/my.cnf"
   fi
-  #
+
+  # ft_stopword_file : Set custom stopword_file for full-text search (empty)
+  grep ft_stopword_file 1>/dev/null 2>/dev/null /etc/mysql/my.cnf
+
+  if [ "$?" != "0" ] ; then
+    echo "[`date +'%Y-%m-%d %H:%M:%S'`] Set ft_stopword_file='mysql_ft_stopword_file.txt'..."
+    my_backup=/etc/mysql/my.cnf.`date +'%Y%m%d%H%M%S'`
+    sudo cp /etc/mysql/my.cnf $my_backup || handle_error "Can't backup my.cnf!"
+    # Append ft_stopword_file='mysql_ft_stopword_file.txt' after [mysqld]
+    sudo sed -e '/^\[mysqld\]/aft_stopword_file="/etc/mysql/mysql_ft_stopword_file.txt"' $my_backup > /tmp/my.cnf.new || handle_error "Can't add ft_min_word_len to /tmp/my.cnf.new!"
+    sudo cp `dirname $0`/mysql_ft_stopword_file.txt /etc/mysql/mysql_ft_stopword_file.txt || handle_error "Can't copy mysql_ft_stopword_file.txt to /etc/mysql/mysql_ft_stopword_file.txt!"
+    sudo cp /tmp/my.cnf.new /etc/mysql/my.cnf || handle_error "Can't copy /tmp/my.cnf.new to my.cnf!"
+    # Restart MySQL
+    sudo service mysql restart
+  else
+    echo "[`date +'%Y-%m-%d %H:%M:%S'`] ft_min_word_len already configured in /etc/mysql/my.cnf"
+  fi
+
 fi
 
 # Change directory to be able to find .sql files below to execute them
