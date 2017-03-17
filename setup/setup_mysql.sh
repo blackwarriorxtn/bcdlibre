20e5d42431740f6950fde932ac8b31f07ce78d43
e-dot <e@kartmann.org>
2017-03-17 18:57:47 +0100

    try to fix seutp on ubuntu (mysql 5.7 has option ONLY_FULL_GROUP_BY!

--- setup/setup_mysql.sh
+++ setup/setup_mysql.sh
@@ -57,6 +57,25 @@ if [ -d /etc/mysql ] ; then
     echo "[`date +'%Y-%m-%d %H:%M:%S'`] ft_min_word_len already configured in ${MYSQL_CONF}"
   fi
 
+  # sql_mode : remove 'ONLY_FULL_GROUP_BY' option
+  # set mode to : STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION
+  # (which is basically MySQL 5.7's defaults except that we remove ONLY_FULL_GROUP_BY and replace STRICT_TRANS_TABLES by STRICT_ALL_TABLES)
+  grep -i "sql_mode='STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'" 1>/dev/null 2>/dev/null ${MYSQL_CONF}
+
+  if [ "$?" != "0" ] ; then
+    echo "[`date +'%Y-%m-%d %H:%M:%S'`] Set sql_mode='STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'..."
+    my_backup=${MYSQL_CONF}.`date +'%Y%m%d%H%M%S'`
+    sudo cp ${MYSQL_CONF} $my_backup || handle_error "Can't backup my.cnf!"
+    # Remove existing sql_mode, if any
+    # Append sql_mode after [mysqld]
+    sudo sed -e "/^\[mysqld\]/asql_mode='STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'" $my_backup > /tmp/my.cnf.new || handle_error "Can't add sql_mode to /tmp/my.cnf.new!"
+    sudo cp /tmp/my.cnf.new ${MYSQL_CONF} || handle_error "Can't copy /tmp/my.cnf.new to my.cnf!"
+    # Restart MySQL
+    sudo service mysql restart
+  else
+    echo "[`date +'%Y-%m-%d %H:%M:%S'`] sql_mode already configured in ${MYSQL_CONF}"
+  fi
+
 fi # if [ -d /etc/mysql ]
 
 cd `dirname $0`/..
