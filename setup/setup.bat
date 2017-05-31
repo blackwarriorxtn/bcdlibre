883f63f4e04707339d0d03d816b558c4eeeed4e7
e-dot <e@kartmann.org>
2017-05-31 17:24:17 +0200

    prepare setup for mysql on windows

--- setup/setup.bat
+++ setup/setup.bat
@@ -22,6 +22,12 @@ IF "%MYSQL_CREATE_SAMPLE%" == "Y" SET MYSQL_CREATE_SAMPLE=y
 
 %COMSPEC% /C %~dp0setup_node_modules.bat || GOTO ERROR
 
+REM TODO setup_mysql.bat
+REM configure MySQL: (my.ini)
+REM   ft_min_word_len=1
+REM   ft_stopword_file= empty or based on system default language list
+REM   sql_mode='STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'
+
 ECHO [%DATE% %TIME%] Create MySQL database...
 mysql --default-character-set=utf8 --user=root --password=%MYSQL_ROOT_PASSWORD% < db/create_database.sql || GOTO ERROR
 IF NOT "%MYSQL_CREATE_SAMPLE%" == "y" GOTO POST_CREATE_SAMPLE
